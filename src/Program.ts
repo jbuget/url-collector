a8aeb2c48c3c14b48c33e2d5851fb4d858ae1e5a
jbuget <jbuget@gmail.com>
2021-10-06 20:59:38 +0200

    Dynamically manage program version
    This uses npm `prebuild` hook. An effort will must be do to take it into account during versionning and publishing.

--- src/Program.ts
+++ src/Program.ts
@@ -1,11 +1,12 @@
 import { Command } from 'commander';
+import { LIB_VERSION } from './version';
 
 export default class Program {
   private readonly _version: string;
   private readonly _command: Command;
 
   constructor() {
-    this._version = '0.0.1';
+    this._version = LIB_VERSION;
     this._command = new Command();
     this._command.version(this._version);
   }
